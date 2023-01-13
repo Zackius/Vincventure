e50754c170417bd6544e7aa1f13e67bc4c3308f2
Zacharia <ndunguzachary24@gmail.com>
2023-01-13 13:59:26 +0300

    Products can be added to cart with their detais: prices and title

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,49 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { RiDeleteBin5Line } from "react-icons/ri"
-import { AiOutlineMinus } from "react-icons/ai"
-import {AiOutlinePlus} from "react-icons/ai"
+import { RiDeleteBin5Line } from "react-icons/ri";
+import { AiOutlineMinus } from "react-icons/ai";
+import { AiOutlinePlus } from "react-icons/ai";
 
 const CartItem = ({ item }) => {
   const { id, title, image, price, amount } = item;
   return (
-    <div className="flex">
+    <div className="flex gap-x-4 py-2 lg:px-2 border-b border-gray-200 w-full font-light text-gray-700">
       <div className="w-full min-h-[150px] flex items-center">
         <Link to={`/product/${id}`}>
           <img className="max-w-[90px]" src={image} alt="Product" />
         </Link>
-        <div  className="w-full flex flex-col pl-4">
+        <div className="w-full flex flex-col">
           <div className="flex justify-between mb-2">
             <Link
               to={`/product/${id}`}
               className="text-sm uppercase max-w-[240px] hover:underline"
             >
               {title}
-            </Link> 
+            </Link>
             <div className="text-xl cursor-pointer">
-              <RiDeleteBin5Line className="text-gray-400 hover:text-red-500 transition duration-150"/>
+              <RiDeleteBin5Line className="text-gray-400 hover:text-red-500 transition duration-150" />
             </div>
           </div>
-          <div className="bg-blue-200 flex gap-x-2 h-[36px] text-sm">
-            <div >
-              <div>
-              <AiOutlineMinus className="text-black hover:text-red-400 transition duration-100 items-center" />
+          <div className=" flex gap-x-2 h-[36px] text-sm">
+            <div className="flex flex-1 max-w-[100px] items-center h-full border  font-medium">
+              <div className="flex-1 h-full flex justify-center items-center cursor-pointer">
+                <AiOutlineMinus className="text-black hover:text-red-400 transition duration-100 items-center" />
               </div>
-              <div>
-       {amount}
-              </div>
-              <div>
+              <div className="-full flex justify-center items-center px-2">{amount}</div>
+              <div className="flex-1 h-full flex justify-center items-center cursor-pointer ">
                 <AiOutlinePlus className=" text-black hover:text-blue-500 transition duration-150" />
               </div>
-             
+            </div>
+            
+            <div className="flex-1 max-w-[100px] justify-around items-center  font-medium">$ {price }</div>
+            <div className="flex-1 max-w-[100px] justify-around items-center  font-bold "> $ {` ${parseFloat(item.price * item.amount).toFixed(2)}`}</div>
+           
           </div>
-          <div>item Price</div>
-          <div>Final Price</div>
         </div>
-        </div>
-        
       </div>
-      
     </div>
   );
 };
