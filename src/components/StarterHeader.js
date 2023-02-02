a6191995f47527d5c6ed70ba14ee04e73240120f
Zacharia <ndunguzachary24@gmail.com>
2023-02-02 14:42:26 +0300

    Solar Lamp responsiveness

--- src/components/StarterHeader.js
+++ src/components/StarterHeader.js
@@ -3,7 +3,7 @@ import React from 'react'
 const StarterHeader = () => {
   return (
       <div className='bg-yellow-300 w-full h-12 border-b-8'>
-      <p className='font-bold text-center p-3'> Need Help placing  orders? Call us  0716 536 879</p>
+      <p className='font-bold text-center text-sm md:text-lg p-3'> Need Help placing  orders? Call us  0742 347 444</p>
       </div>
   )
 }
--- src/pages/SolarLamp.js
+++ src/pages/SolarLamp.js
@@ -22,13 +22,13 @@ export const SolarLamp = () => {
     <section className='py-32 content-center'>
       <section>
         <div className='text-center'>
-        <h2 className='text-4xl font-bold text-red-600 capitalize text-center underline'>Solar street Lamp</h2>
+        <h2 className='text-2xl font-bold md:text-4xl text-red-600 capitalize text-center underline'>Solar street Lamp</h2>
           <p className='text-xl font-semibold'>200w Solar LED Street lamp, All weather resistance, auto switch off during the day and on at night. Long lasting solar lamp. </p>
           <div className='hero container max-w-screen-lg mx-auto pb-10'>
           <img className='mx-auto'  src={Solar2} alt="/" />
           </div>
           <div className='text-center'>
-          <h2 className='text-3xl font-bold text-red-600 capitalize p-3 underline '>Lamp Specification</h2>
+          <h2 className='text-xl md:text-2xl font-bold text-red-600 capitalize p-3 underline '>Lamp Specification</h2>
           <ul className='font-semibold text-xl'>
             <li>Battery type 8000MAH</li>
             <li>6-8 Hours  Charging time.</li>
@@ -42,14 +42,14 @@ export const SolarLamp = () => {
           
         </div>
         <div className='py-6'>
-          <h2 className='text-4xl font-bold text-red-600 capitalize text-center underline '>Bright LEDS</h2>
+          <h2 className='text-2xl font-bold md:text-4xl text-red-600 capitalize text-center underline'>Bright LEDS</h2>
           <div className='hero container max-w-screen-lg mx-auto pb-10'>
           <img className='mx-auto'  src={Solar} alt="/" />
           </div>
           <p  className='text-lg font-semibold text-center' >Super bright  LEDS up to 200W</p>
         </div>
         <div className='py-6'>
-          <h2 className='text-4xl font-bold text-red-600 capitalize text-center underline '>SOLAR PANEL</h2>
+          <h2 className='text-2xl font-bold md:text-4xl text-red-600 capitalize text-center underline'>SOLAR PANEL</h2>
           <div className='hero container max-w-screen-lg mx-auto pb-10'>
           <img className='mx-auto'  src={Panel} alt="/" />
           </div>
@@ -58,14 +58,14 @@ export const SolarLamp = () => {
 
       </section>
       <section className='py-8 hero container max-w-screen-lg mx-auto pb-10'>
-        <h2 className='text-4xl font-bold  text-center text-red-600 capitalize p-6 underline '>PRICING & DELIVERY</h2>
-        <div className='grid grid-cols-2 gap-5  h-44'>
-          <div className= 'border-2 border-yellow-400 justify-center'>
-          <h1 className='text-center text-2xl font-bold p-20 line-through'>Normal Price KES 4999</h1>
+        <h2 className='text-2xl font-bold md:text-4xl text-red-600 capitalize text-center underline p-4'>PRICING & DELIVERY</h2>
+        <div className='grid grid-cols-2 gap-5 '>
+          <div className= 'border-2 border-yellow-400 mx-auto md:w-[400px] md:h-[200px] md:p-20 '>
+          <h1 className='text-center text-sm p-4 font-bold  line-through place-content-center md:text-2xl  '>Normal Price KES 4500</h1>
           </div>
-          <div className='border-2 border-red-600'>
-            <p className='text-center text-2xl font-bold p-16 '>OFFER PRICE KES 3500 <br/>
-       OFFER PRICE 2pcs KES 6500</p>
+          <div className='border-2 border-red-600 mx-auto md:w-[400px] md:h-[200px] md:p-12' >
+            <p className='text-center text-sm font-semibold  md:text-2xl  md:font-bold '>OFFER PRICE KES 2,999<br/>
+       OFFER PRICE 2pcs KES 5500</p>
           </div>
         </div>
         <div className='py-12 text-center  content-center'>
@@ -81,33 +81,33 @@ export const SolarLamp = () => {
         <div className='mx-auto'>
         <div className='flex flex-col'>
         <label className='text-xl font-bold'>Name <span className='text-red-600'>*</span></label> 
-      <input className='appearance-none block  w-[500px] bg-gray-200 text-gray-500 border  rounded-2xl  py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white' type="text" name="user_name" placeholder='John Doe' required />
+      <input className='appearance-none block  w-[300px]  md:w-[500px] bg-gray-200 text-gray-500 border  rounded-2xl  py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white' type="text" name="user_name" placeholder='John Doe' required />
         </div>
         <div>
         <label className='text-xl font-bold'>Phone Number <span className='text-red-600'>*</span></label>
-      <input className='appearance-none block w-[500px] bg-gray-200 text-gray-500 border  rounded-2xl  py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white' type="integer" name="phone_number" placeholder=" +254"required />
+      <input className='appearance-none block w-[300px] md:w-[500px] bg-gray-200 text-gray-500 border  rounded-2xl  py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white' type="integer" name="phone_number" placeholder=" +254"required />
      </div>
         <div>
         <label className='text-xl font-bold'>Delivery Location  <span className='text-red-600'>*</span> </label>
-      <textarea className='appearance-none block w-[500px] bg-gray-200 text-gray-500 border  rounded-2xl  py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white '  name="delivery_location" placeholder='Nairobi, Kahawa west'  required/>
+      <textarea className='appearance-none block w-[300px] md:w-[500px] bg-gray-200 text-gray-500 border  rounded-2xl  py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white '  name="delivery_location" placeholder='Nairobi, Kahawa west'  required/>
           </div>
           <div>
             <label className='text-xl font-bold'>Select Your Offer  <span className='text-red-600'>*</span> </label>
 
             <div className='flex flex-rows gap-4'>
-                <input type="checkbox" name='1piece_3500' /><p>1 PC AT KES. 3,500</p> 
+                <input type="checkbox" name='solar_lamp'  value="1 PC AT KES. 3,500"/><p>1 PC AT KES. 2,999</p> 
             </div>
             <div className='flex flex-rows gap-4'>
-            <input type="checkbox" name ="2pieces_6500"/><p>2PC AT KES. 6500</p>
+            <input type="checkbox" name ="solar_lamp" value="2PC AT KES. 6500"/><p>2PC AT KES. 6500</p>
   </div>
           
           </div>
           <div>
         <label className='text-xl font-bold'>Optional Note  </label>
-      <textarea className='appearance-none block w-[500px] bg-gray-200 text-gray-500 border  rounded-2xl  py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white ' type="text"  name="optional_note" />
+      <textarea className='appearance-none block w-[300px] md:w-[500px] bg-gray-200 text-gray-500 border  rounded-2xl  py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white ' type="text"  name="optional_note" />
           </div>
         <div>
-          <button className='bg-yellow-400 rounded-xl hover:bg-yellow-600'>
+          <button className= ' content-center bg-yellow-400 rounded-xl hover:bg-yellow-600'>
           <input className='p-4' type="submit" value="Place Order" />
           </button>
           </div>
