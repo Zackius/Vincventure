9eefcd87a3e516745f5fcd010700043f858dbb85
Zacharia <ndunguzachary24@gmail.com>
2023-02-13 13:12:28 +0300

    polishing application

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import {Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
